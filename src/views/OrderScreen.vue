6c29a929ed40016f417ce30f84d30b4f96ff47cd
smmrlg <smmrlgg@gmail.com>
2026-05-25 17:36:40 +0300

    Pagination exercise

--- src/views/OrderScreen.vue
+++ src/views/OrderScreen.vue
@@ -1,9 +1,23 @@
 <script setup> 
+import CarCard from '@/components/CarCard.vue';
 import LeadForm from '@/components/LeadForm.vue';
-
 
 </script>
 
 <template>
+        
+        <CarCard/>
         <LeadForm/>
+       <!-- <CarCard 
+        v-for = "car in cars"
+        :key = "car.id"
+        :model = "car.model"
+        :manufacturer="car.manufacturer"
+        :image="car.image"
+        :description="car.description"
+        :button = "car.button"
+        /> -->
+        
+       
+        
 </template>
