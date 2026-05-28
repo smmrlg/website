a9e253eea4f463c6e0d8ece7578d37ca24ab31fa
smmrlg <smmrlgg@gmail.com>
2026-05-28 14:50:28 +0300

    Working on a calendar

---
+++ src/components/Calendar.vue
@@ -0,0 +1,14 @@
+<script setup>
+const date = new Date()
+const monthNames = ["Январь",  "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"]
+const currentYear = date.getFullYear()
+
+</script>
+
+<template>
+    <p> {{ currentYear }} </p>
+
+
+
+
+</template>
--- src/views/LoginScreen.vue
+++ src/views/LoginScreen.vue
@@ -1,4 +1,11 @@
+<script>
+import Calendar from '@/components/Calendar.vue';
+
+</script>
+
+
 <template>
         <h1> Заготовка для LoginScreen.vue </h1>
+        <Calendar/>
 
 </template>
