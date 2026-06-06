44ce35a268577ad22812c6632bd4377510a18960
Deword <forlidzen@gmail.com>
2026-06-06 20:37:13 +0300

    draft to beauty

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,16 @@
 <script setup>
 import Menu from './components/Menu.vue';
 import Header from './components/Header.vue';
+import './App.css';
 </script>
 
 <template>
   <Header/>
-  <router-view/>
-  
-
-   
+  <Menu/> 
+  <main class="main-content">
+    <router-view />
+  </main>
 </template>
 
 <style> 
-* {   /* Сбрасываем дефолтное форматирование и подкидываем нормальный шрифт */
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif;
-}
 </style>
---
+++ src/views/BeautyMainScreen.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { ref } from "vue";
+const isSidebarOpen = ref(false);
+</script>
+
+<template>
+    <div class="screen-container">
+        <div class="content-left">
+            <h1>"Ifur Kavua"</h1>
+            <p>Welcome!</p>
+            <p>My name is Daria, I’m a sertified PMU artist in Israel.</p>
+            <p>Natural lips&brows enhancement.</p>
+            <p>Personal approach, safe healing, natural result.</p>
+            <p>
+                 ✨ I’ll guide you calmly and clearly — so you feel confident
+                and know what’s best for you💫
+            </p>
+            <button @click="isSidebarOpen = true" class="menu-btn">
+                Открыть меню
+            </button>
+        </div>
+        <div :class="['sidebar', { 'sidebar-active': isSidebarOpen }]">
+            <button @click="isSidebarOpen = false" class="close-btn">
+                &times;
+            </button>
+            <nav class="sidebar-links">
+                <a href="#">Услуги</a>
+                <a href="#">Мастера</a>
+                <a href="#">Контакты</a>
+            </nav>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+@import "@/views/css/Beauty.css";
+</style>
